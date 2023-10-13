0fe1576468af754ffd56e0f84cfb569173892cba
Kuenefe <emrecemguney@gmail.com>
2023-10-14 00:54:35 +0200

    goind strong

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    let mut single_apple_price: u32 = 2;
+
+    if amount > 40 {
+        single_apple_price = 1;
+        return amount * single_apple_price;
+    }
+    single_apple_price * amount
+}
 
 // Don't modify this function!
 #[test]
